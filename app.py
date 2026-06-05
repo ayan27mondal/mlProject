a29ce5eeca478faf6c0f8efae71e178ad8913e38
ayan27mondal <ayan27business@gmail.com>
2026-06-05 17:24:48 +0530

    Prediction Pipeline Completed

---
+++ app.py
@@ -0,0 +1,42 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+from src.exception import CustomException
+from src.logger import logging
+import numpy as np
+import pandas as pd
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def home():
+    return render_template('index.html')
+  
+@application.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+      return render_template('home.html')
+    else:
+      # Handle POST request for prediction
+      data = CustomData(
+        gender = request.form.get('gender'),
+        race_ethnicity= request.form.get('race_ethnicity'),
+        parental_level_of_education= request.form.get('parental_level_of_education'),
+        lunch= request.form.get('lunch'),
+        test_preparation_course= request.form.get('test_preparation_course'),
+        reading_score= int(request.form.get('reading_score')),
+        writing_score= int(request.form.get('writing_score')), 
+      )
+      pred_df = data.get_data_as_dataframe()
+      print(pred_df)
+      
+      predict_pipeline = PredictPipeline()
+      results = predict_pipeline.predict(pred_df)
+      if results[0] >= 100:
+        results[0] = 100
+      return render_template('home.html', results=results[0])
+    
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", debug=True)
