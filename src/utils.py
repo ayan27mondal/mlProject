3a44d59c13bfccf44ce2a63ac3563bb88c330e39
ayan27mondal <ayan27business@gmail.com>
2026-06-05 14:22:11 +0530

    Model Trained with Hyperparameter Turningg

--- src/utils.py
+++ src/utils.py
@@ -36,6 +36,8 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
       
       model.fit(X_train, y_train) # Train model
       
+      # model.fit(X_train, y_train) # Train model
+      
       y_train_pred = model.predict(X_train)
 
       y_test_pred = model.predict(X_test)
